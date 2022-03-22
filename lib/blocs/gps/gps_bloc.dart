15b58add73ccd771f549f4225ea125c9c83ae4d0
Jozek Hajduk <hajduksanchez.dev@gmail.com>
2022-03-22 06:52:48 -0500

    🚧 [ADD] Listener of GPS

--- lib/blocs/gps/gps_bloc.dart
+++ lib/blocs/gps/gps_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:geolocator/geolocator.dart';
 
 part 'gps_event.dart';
 part 'gps_state.dart';
@@ -11,5 +12,20 @@ class GpsBloc extends Bloc<GpsEvent, GpsState> {
     on<GpsAndPermissionEvent>((event, emit) => emit(state.copyWith(
         isGpsEnabled: event.isGpsEnabled,
         isGpsPermissionGranted: event.isGpsPermissionGranted)));
+    _init();
+  }
+
+  Future<void> _init() async {
+    final isEnable = await _checkGpsStatus();
+    print("GPS is enable: $isEnable");
+  }
+
+  Future<bool> _checkGpsStatus() async {
+    final isEnable = await Geolocator.isLocationServiceEnabled();
+    // This one is the listener for the GPS status chenges
+    Geolocator.getServiceStatusStream().listen((event) {
+      print("status: ${event.toString()}");
+    });
+    return isEnable;
   }
 }
