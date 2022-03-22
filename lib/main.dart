ca8846b539bd84bb3a4fd3f1214016605c9d79a3
Jozek Hajduk <hajduksanchez.dev@gmail.com>
2022-03-21 19:17:36 -0500

    🚧 [ADD] Directory structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:maps_app/screens/screens.dart';
 
 void main() => runApp(const MyApp());
 
@@ -10,11 +11,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Maps App',
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World'),
-        ),
-      ),
+      home: Scaffold(body: LoadingScreen()),
     );
   }
 }
---
+++ lib/screens/screens.dart
@@ -0,0 +1,5 @@
+// This file works as the Index file for centralize all the screens.
+
+export 'package:maps_app/screens/gps_access_screen.dart';
+export 'package:maps_app/screens/map_screen.dart';
+export 'package:maps_app/screens/loading_screen.dart';
