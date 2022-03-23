de59c8fdc05bba3b39bcec618ad6815ac4616f71
Jozek Hajduk <hajduksanchez.dev@gmail.com>
2022-03-23 07:15:00 -0500

    🚧 [ADD] Get current position

--- lib/blocs/location/location_bloc.dart
+++ lib/blocs/location/location_bloc.dart
@@ -1,13 +1,37 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:geolocator/geolocator.dart';
 
 part 'location_event.dart';
 part 'location_state.dart';
 
 class LocationBloc extends Bloc<LocationEvent, LocationState> {
+  StreamSubscription? _positionStream;
+
   LocationBloc() : super(const LocationState()) {
     on<LocationEvent>((event, emit) {
       // TODO: implement event handler
     });
+  }
+
+  Future getCurrentPosition() async {
+    final location = await Geolocator.getCurrentPosition();
+    // TODO: Return object type LatLng from Google maps
+  }
+
+  void startFollowingUser() {
+    _positionStream = Geolocator.getPositionStream().listen((event) {});
+  }
+
+  void stopFollowingUser() {
+    _positionStream?.cancel(); // Close the stream
+  }
+
+  @override
+  Future<void> close() {
+    stopFollowingUser();
+    return super.close();
   }
 }
--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,7 +1,32 @@
 import 'package:flutter/material.dart';
 
-class MapScreen extends StatelessWidget {
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:maps_app/blocs/blocs.dart';
+
+class MapScreen extends StatefulWidget {
   const MapScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  late LocationBloc _locationBloc;
+
+  @override
+  void initState() {
+    super.initState();
+    _locationBloc = BlocProvider.of<LocationBloc>(context);
+    _locationBloc.getCurrentPosition();
+    _locationBloc.startFollowingUser();
+  }
+
+  @override
+  void dispose() {
+    _locationBloc.stopFollowingUser();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
