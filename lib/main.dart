a16c3e952db299e83e4fcff2531885fdd023ddcf
Jozek Hajduk <hajduksanchez.dev@gmail.com>
2022-03-29 15:21:40 -0500

    🚧 [ADD] Search BLoC

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,8 @@ void main() {
       BlocProvider(create: (context) => GpsBloc()),
       BlocProvider(create: (context) => LocationBloc()),
       BlocProvider(
-          create: (context) =>
-              MapBloc(locationBloc: BlocProvider.of<LocationBloc>(context))),
+          create: (context) => MapBloc(locationBloc: BlocProvider.of<LocationBloc>(context))),
+      BlocProvider(create: (context) => SearchBloc()),
     ],
     child: const MyApp(),
   ));
