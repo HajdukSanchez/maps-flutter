e734400f7ef89d2f6f4d807b8b76aa191e0a78cf
Jozek Hajduk <hajduksanchez.dev@gmail.com>
2022-03-29 15:59:32 -0500

    🚧 [ADD] SearchBox

--- lib/widgets/search_bar.dart
+++ lib/widgets/search_bar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:maps_app/blocs/blocs.dart';
@@ -21,45 +22,48 @@ class SearchBar extends StatelessWidget {
 class _SearchBarBody extends StatelessWidget {
   const _SearchBarBody({Key? key}) : super(key: key);
 
-  onSearchResults(BuildContext context, SearchResult result) {
-    if (result.manual) {
-      BlocProvider.of<SearchBloc>(context).add(OnActivateManualMarkerEvent());
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Container(
-        margin: const EdgeInsets.only(top: 10, left: 20, right: 20),
-        width: double.infinity,
-        child: GestureDetector(
-          onTap: () async {
-            final result =
-                await showSearch(context: context, delegate: SearchDestinationDelegate());
-            if (result == null) return;
-            onSearchResults(context, result);
-          },
-          child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-            child: const Text(
-              "Where to ?",
-              style: TextStyle(color: Colors.black87, fontSize: 16),
-            ),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(10),
-              boxShadow: const [
-                BoxShadow(
-                  color: Colors.black12,
-                  blurRadius: 5,
-                  offset: Offset(0, 10),
-                ),
-              ],
+      child: FadeInDown(
+        duration: const Duration(milliseconds: 300),
+        child: Container(
+          margin: const EdgeInsets.only(top: 10, left: 20, right: 20),
+          width: double.infinity,
+          child: GestureDetector(
+            onTap: () async {
+              final result =
+                  await showSearch(context: context, delegate: SearchDestinationDelegate());
+              if (result == null) return;
+              onSearchResults(context, result);
+            },
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              child: const Text(
+                "Where to ?",
+                style: TextStyle(color: Colors.black87, fontSize: 16),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.black12,
+                    blurRadius: 5,
+                    offset: Offset(0, 10),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
       ),
     );
+  }
+
+  void onSearchResults(BuildContext context, SearchResult result) {
+    if (result.manual) {
+      BlocProvider.of<SearchBloc>(context).add(OnActivateManualMarkerEvent());
+    }
   }
 }
